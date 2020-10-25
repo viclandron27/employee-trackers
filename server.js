31bdfbbc5e51355d864c0c163b0afbdb2427176d
Victoria Landron <viclandron@gmail.com>
2020-10-25 12:42:57 -0400

    added inquirer with prompts and got all employees to show

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql2');
+const inquirer = require('inquirer');
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -10,16 +11,56 @@ const connection = mysql.createConnection({
     database: 'employeeDB'
   });
 
-  connection.connect(err => {
+connection.connect(err => {
     //if (err) throw err;
     console.log('connected as id ' + connection.threadId);
     afterConnection();
-  });
+});
   
-  afterConnection = () => {
-    connection.query('SELECT * employee', function(err, res) {
-      //if (err) throw err;
-      console.log(res);
-      connection.end();
-    });
-  };
+afterConnection = () => {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "userChoice",
+            message: "What would you like to do?",
+            choices: ['View All Employees', 'View All Employees By Department',
+            'View All Employees By Manager', 'Add Employee', 'Remove Employee', 
+            'Update Employee Role', 'Update Employee Manager']
+        },
+    ])
+    .then(userChoice => {
+        //console.log("userChoice inside of createTeam(): ", userChoice);
+
+        switch (userChoice.userChoice) {
+            case "View All Employees":
+                allEmployees();
+                break;
+            case "View All Employees By Department":
+                byDepartment();
+                break;
+            case "View All Employees By Manager":
+                byManager();
+                break;
+            case "Add Employe":
+                addEmployee();
+                break;
+            case "Remove Employee":
+                removeEmployee();
+                break;
+            case "Update Employee Role":
+                updateEmployeeRole();
+                break;
+            case "Update Employee Manager":
+                updateEmployeeManager();
+                break;
+        }
+    })
+};
+
+function allEmployees() {
+    connection.query('SELECT * FROM employee', function(err, res) {
+        //if (err) throw err;
+        console.log(res);
+        connection.end();
+      });
+}
