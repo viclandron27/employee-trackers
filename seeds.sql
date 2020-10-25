02a305d5ccb53c89ec80f48762b4c991ed73ae43
Victoria Landron <viclandron@gmail.com>
2020-10-25 12:16:09 -0400

    populated all the tables

--- seeds.sql
+++ seeds.sql
@@ -4,3 +4,16 @@ VALUES
 (2, 'Susie', 'Sanders', 1, 1),
 (3, 'Zack', 'Martin', 3, 3);
 
+INSERT INTO role (id, title, salary, department_id)
+VALUES
+(1, 'Teacher', 30.2, 1),
+(2, 'Principal', 60.1, 2),
+(3, 'Nurse', 40.3, 4);
+
+INSERT INTO department (id, name)
+VALUES
+(1, 'Elementary School'),
+(2, 'Middle School'),
+(3, 'High School'),
+(4, 'Entire School')
+
